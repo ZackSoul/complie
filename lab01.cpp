56328f53dfccc51d5dba49b9cd87b2cdb4273b8a
neverDeady <91828262+neverDeady@users.noreply.github.com>
2021-10-03 19:19:05 +0800

    Update lab01.cpp

--- lab01.cpp
+++ lab01.cpp
@@ -10,8 +10,8 @@ using namespace std;
 
 int main(int argc,char** argv){
     int a,b;
-    a = argv[1];
-    b = argv[2];
+    a = argv[1] - '0';
+    b = argv[2] - '0';
     printf("%d",a+b);
     return 0;
 }
