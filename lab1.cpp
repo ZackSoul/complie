9724fc55f559ba255864ccd1bbf9c852394569f1
neverDeady <91828262+neverDeady@users.noreply.github.com>
2021-10-24 10:29:15 +0800

    Update lab1.cpp

--- lab1.cpp
+++ lab1.cpp
@@ -177,9 +177,9 @@ void lexerAnalyse(FILE *fpin){
                 //     fseek(fpin,-1L,SEEK_CUR);
                 // }
                 else{
-                    sym[sym_p++] = 4;
-                    word[word_p++] = token;
-                    fseek(fpin,-1L,SEEK_CUR);
+//                     sym[sym_p++] = 4;
+//                     word[word_p++] = token;
+//                     fseek(fpin,-1L,SEEK_CUR);
                     // printf("numtype error\n");
 //                     exit(-1);
                 }
