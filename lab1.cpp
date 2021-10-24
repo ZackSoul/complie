cd70d71186bd65f6aad83b0317014f86e717d847
neverDeady <91828262+neverDeady@users.noreply.github.com>
2021-10-24 10:54:07 +0800

    Update lab1.cpp

--- lab1.cpp
+++ lab1.cpp
@@ -211,13 +211,13 @@ void lexerAnalyse(FILE *fpin){
                         }
                         else if(ch == EOF){
                             // printf("last\n");
-                            exit(-1);
+//                             exit(-1);
                         }
                         ch = fgetc(fpin);
                     }
                     else if(ch == EOF){
                         // printf("last\n");
-                        exit(-1);
+//                         exit(-1);
                     }
                     else{
                         ch = fgetc(fpin);
@@ -226,7 +226,7 @@ void lexerAnalyse(FILE *fpin){
             }
             else{
                 // printf("lexer error\n");
-                exit(-1);
+//                 exit(-1);
             }
         }
         else switch(ch){
