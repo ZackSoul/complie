70a788b4695ca115cca468e679cf1a6c157fe7ed
neverDeady <91828262+neverDeady@users.noreply.github.com>
2021-10-29 09:39:20 +0800

    Update and rename add.cpp to Main.java

---
+++ Main.java
@@ -0,0 +1,36 @@
+package lab1;
+
+import java.io.*;
+import java.util.ArrayList;
+
+public class Main {
+    public static String s;
+    public static ArrayList<Word> words = new ArrayList<>();
+    public static ArrayList<Word> syms = new ArrayList<>();
+    public static void main(String[] args) throws IOException {
+//        InputStreamReader fpin = new InputStreamReader(new FileInputStream(args[0]));
+        InputStreamReader fpin = new InputStreamReader(new FileInputStream("E:\\JavaFile\\compile\\src\\lab1\\in.txt"));
+        StringBuilder str = new StringBuilder();
+        int flag;
+        while((flag = fpin.read()) != -1){
+            str.append((char) flag);
+        }
+        fpin.close();
+        s = str.toString();
+        Lexer.lexerAnalyse(s);
+//        for(int i = 0; i < words.size(); i++){
+//            System.out.print(words.get(i).getWord());
+//        }
+        Parse.parseAnalyse();
+        StringBuilder sout = new StringBuilder();
+        for(int i = 0; i < words.size(); i++){
+            sout.append(words.get(i).getOutPut());
+        }
+        String out = sout.toString();
+//        System.out.println(out);
+        OutputStreamWriter fout = new OutputStreamWriter(new FileOutputStream(args[1]));
+//        OutputStreamWriter fout = new OutputStreamWriter(new FileOutputStream("E:\\JavaFile\\compile\\src\\lab1\\out.txt"));
+        fout.write(out);
+        fout.close();
+    }
+}
