a1e11a24a1986f968c5b40f62e615b03faf16077
neverDeady <91828262+neverDeady@users.noreply.github.com>
2021-10-31 00:03:00 +0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,11 +2,13 @@
 
 import java.io.*;
 import java.util.ArrayList;
+import java.util.Stack;
 
 public class Main {
     public static String s;
     public static ArrayList<Word> words = new ArrayList<>();
     public static ArrayList<Word> syms = new ArrayList<>();
+    public static Stack<Integer> stack = new Stack<>();
     public static void main(String[] args) throws IOException {
        InputStreamReader fpin = new InputStreamReader(new FileInputStream(args[0]));
 //         InputStreamReader fpin = new InputStreamReader(new FileInputStream("E:\\JavaFile\\compile\\src\\lab2\\in.txt"));
@@ -23,38 +25,14 @@ public class Main {
 //        }
         Parse.parseAnalyse();
         StringBuilder sout = new StringBuilder();
-        int num = 1;
-        int cnt = 0;
-        for(int i = 0; i < words.size(); i++){
-            while(words.get(i).getType()==10||words.get(i).getType()==11){
-                if(words.get(i).getType()==10){
-                    num *= -1;
-                }
-                else{
-                    num *= 1;
-                }
-                i++;
-            }
-            if(words.get(i).getType()==4){
-                num = num * Integer.valueOf(words.get(i).getOutPut());
-                sout.append(String.valueOf(num));
-            }
-           else{
-                if(words.get(i).getType()==5||words.get(i).getType()==6){
-                    cnt++;
-                    if(cnt <= 2){
-                        sout.append(words.get(i).getOutPut());
-                    }
-                }
-                else{
-                    sout.append(words.get(i).getOutPut());
-                }
-           }
-        }
+        sout.append("define dso_local i32 @main(){\n");
+        sout.append("\tret i32 ");
+        sout.append(stack.peek());
+        sout.append("\n}");
         String out = sout.toString();
 //        System.out.println(out);
        OutputStreamWriter fout = new OutputStreamWriter(new FileOutputStream(args[1]));
-//        OutputStreamWriter fout = new OutputStreamWriter(new FileOutputStream("E:\\JavaFile\\compile\\src\\lab2\\out.txt"));
+//         OutputStreamWriter fout = new OutputStreamWriter(new FileOutputStream("E:\\JavaFile\\compile\\src\\lab2\\out.txt"));
         fout.write(out);
         fout.close();
     }
