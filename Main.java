9e465f4ded79b620f958871bbc165714f31a38e0
neverDeady <91828262+neverDeady@users.noreply.github.com>
2021-10-30 12:46:52 +0800

    Update and rename main.cpp to Main.java

---
+++ Main.java
@@ -0,0 +1,52 @@
+package lab2;
+
+import java.io.*;
+import java.util.ArrayList;
+
+public class Main {
+    public static String s;
+    public static ArrayList<Word> words = new ArrayList<>();
+    public static ArrayList<Word> syms = new ArrayList<>();
+    public static void main(String[] args) throws IOException {
+//        InputStreamReader fpin = new InputStreamReader(new FileInputStream(args[0]));
+        InputStreamReader fpin = new InputStreamReader(new FileInputStream("E:\\JavaFile\\compile\\src\\lab2\\in.txt"));
+        StringBuilder str = new StringBuilder();
+        int flag;
+        while((flag = fpin.read()) != -1){
+            str.append((char) flag);
+        }
+        fpin.close();
+        s = str.toString();
+        Lexer.lexerAnalyse(s);
+//        for(int i = 0; i < words.size(); i++){
+//            System.out.print(words.get(i).getWord());
+//        }
+        Parse.parseAnalyse();
+        StringBuilder sout = new StringBuilder();
+        int num = 1;
+        for(int i = 0; i < words.size(); i++){
+            while(words.get(i).getType()==10||words.get(i).getType()==11){
+                if(words.get(i).getType()==10){
+                    num *= -1;
+                }
+                else{
+                    num *= 1;
+                }
+                i++;
+            }
+            if(words.get(i).getType()==4){
+                num = num * Integer.valueOf(words.get(i).getOutPut());
+                sout.append(String.valueOf(num));
+            }
+//            else{
+//                sout.append(words.get(i).getOutPut());
+//            }
+        }
+        String out = sout.toString();
+//        System.out.println(out);
+//        OutputStreamWriter fout = new OutputStreamWriter(new FileOutputStream(args[1]));
+        OutputStreamWriter fout = new OutputStreamWriter(new FileOutputStream("E:\\JavaFile\\compile\\src\\lab2\\out.txt"));
+        fout.write(out);
+        fout.close();
+    }
+}
