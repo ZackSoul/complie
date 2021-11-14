2144f03bca1f14189aa7ef34622af80606a43e38
neverDeady <91828262+neverDeady@users.noreply.github.com>
2021-11-14 23:54:18 +0800

    Create Register.java

---
+++ Register.java
@@ -0,0 +1,27 @@
+
+
+public class Register {
+    int id;
+    Integer num;
+
+    public Register(int id,Integer num){
+        this.id = id;
+        this.num = num;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setNum(Integer num) {
+        this.num = num;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public Integer getNum() {
+        return num;
+    }
+}
